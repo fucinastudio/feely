6b45ffc5cf8019ced782380ae5be6ab32c9c1fed
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2024-10-16 20:35:25 +0200

    Removed logs and restored metadata

--- app/api/workspace/createWorkspace/route.ts
+++ app/api/workspace/createWorkspace/route.ts
@@ -1,24 +1,23 @@
-'use server';
+"use server";
 
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-import { createWorkspace } from '@/app/api/apiServerActions/workspaceApiServerActions';
-import { authenticateUser } from '@/app/api/apiUtils';
+import { createWorkspace } from "@/app/api/apiServerActions/workspaceApiServerActions";
+import { authenticateUser } from "@/app/api/apiUtils";
 
 //Usage from client
 export async function POST(req: NextRequest) {
   const user = await authenticateUser(req);
   if (!user) {
-    return NextResponse.json({ message: 'Unauthorized user' }, { status: 401 });
+    return NextResponse.json({ message: "Unauthorized user" }, { status: 401 });
   }
   const body = await req.json();
   const { workspaceName } = body;
   const res = await createWorkspace(workspaceName, user.accessToken);
 
-  console.log('Res', res);
   if (res.isSuccess) {
     return NextResponse.json(
-      { message: 'Workspace created', id: res.id, name: workspaceName },
+      { message: "Workspace created", id: res.id, name: workspaceName },
       { status: 200 }
     );
   }
--- app/api/workspace/image/route.ts
+++ app/api/workspace/image/route.ts
@@ -1,27 +1,26 @@
-'use server';
+"use server";
 
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-import { uploadImageForWorkspace } from '@/app/api/apiServerActions/workspaceApiServerActions';
-import { authenticateUser } from '@/app/api/apiUtils';
+import { uploadImageForWorkspace } from "@/app/api/apiServerActions/workspaceApiServerActions";
+import { authenticateUser } from "@/app/api/apiUtils";
 
 export async function POST(req: NextRequest) {
   const user = await authenticateUser(req);
   if (!user) {
-    return NextResponse.json({ message: 'Unauthorized user' }, { status: 401 });
+    return NextResponse.json({ message: "Unauthorized user" }, { status: 401 });
   }
-  console.log('Req', req);
+  console.log("Req", req);
   const body = await req.json();
-  console.log('Body', body);
+  console.log("Body", body);
   const res = await uploadImageForWorkspace({
     ...body,
     access_token: user.accessToken,
   });
 
-  console.log('Res', res);
   if (res.isSuccess) {
     return NextResponse.json(
-      { message: 'Image uploaded created', imageUrl: res.data },
+      { message: "Image uploaded created", imageUrl: res.data },
       { status: 200 }
     );
   }
