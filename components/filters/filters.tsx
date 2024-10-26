aa0e6d73b8c36b8ddb9647880672894106533c97
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2024-10-26 14:43:54 +0200

    Remove filtered statuses in roadmap

--- components/filters/filters.tsx
+++ components/filters/filters.tsx
@@ -1,18 +1,24 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import { IIdeasOrdering } from '@/types/DTO/getIdeasByWorkspaceNameDTO';
-import { StatusType } from '@/types/status';
-import { TopicType } from '@/types/topic';
-import useDebounce from '@/utils/useDebounce';
+import { IIdeasOrdering } from "@/types/DTO/getIdeasByWorkspaceNameDTO";
+import { StatusType } from "@/types/status";
+import { TopicType } from "@/types/topic";
+import useDebounce from "@/utils/useDebounce";
+import { allowedStatuses } from "@/app/[org]/(pages)/roadmap/default_page";
 
 interface IProps {
   topics: TopicType[];
   statuses: StatusType[];
+  roadmapStatuses?: boolean;
 }
 
-const useMainPageFilters = ({ topics, statuses }: IProps) => {
+const useMainPageFilters = ({
+  topics,
+  statuses,
+  roadmapStatuses = false,
+}: IProps) => {
   const {
     mainState: mainSearchTitle,
     slaveState: searchTitleFastRefreshing,
@@ -22,12 +28,14 @@ const useMainPageFilters = ({ topics, statuses }: IProps) => {
   const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
   const [selectedStatuses, setSelectedStatuses] = useState<string[]>([]);
 
-  const [selectedOrder, setSelectedOrder] = useState<IIdeasOrdering>('latest');
+  const [selectedOrder, setSelectedOrder] = useState<IIdeasOrdering>("latest");
 
   return {
     filterObjectAttributes: {
       topics: topics,
-      statuses: statuses,
+      statuses: statuses.filter(
+        (status) => !roadmapStatuses || allowedStatuses.includes(status.name)
+      ),
       setDebounced: setDebounced,
       selectedTopics: selectedTopics,
       setSelectedTopics: setSelectedTopics,
